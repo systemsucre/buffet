3c277e8fea6fe451b4533c138349999540b18188
systemsucre <systemsucre@gmail.com>
2026-02-01 16:21:35 -0400

    fff

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Cambia a '/app/' si va en subcarpeta
+  // base: './', // Cambia a '/app/' si va en subcarpeta
 })
