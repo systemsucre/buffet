3eed94f32affff1b0dc28100acef018280f408a2
systemsucre <systemsucre@gmail.com>
2026-02-01 21:54:48 -0400

    ffff

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,8 @@ export function Footer() {
     return (
         <footer className="bg-dark mt-5 py-5">
             <div className="container-xl">
-                <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+                <p className="text-white text-center fs-4 mt-4 m-md-0">KR Estudios - Todos los derechos Reservados</p>
+                <p className="text-white text-center fs-4 mt-4 m-md-0"> <small style={{fontSize:'10px'}}> ©2026 copyright SITIO WEB</small></p>
             </div>
         </footer>
     )
