294418246ccfda97e1bde60171befa06009b9eb4
systemsucre <systemsucre@gmail.com>
2026-02-03 10:06:27 -0400

    sff

--- src/components/etc/menu.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.has-submenu > a').forEach(item => {
-  item.addEventListener('click', (e) => {
-    if (window.innerWidth <= 992) {
-      e.preventDefault(); // Evita que el link cargue otra página
-      const submenu = item.nextElementSibling;
-      submenu.classList.toggle('show');
-    }
-  });
-});
